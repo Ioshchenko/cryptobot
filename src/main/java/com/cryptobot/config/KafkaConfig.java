62dd640e68a84ecc5d2477853f3747a5fc919afc
Ivan Ioshchenko <iioshchenko@GDKHA6569-wf.local>
2018-11-13 21:38:59 +0200

    Update Kafka config

--- src/main/java/com/cryptobot/config/KafkaConfig.java
+++ src/main/java/com/cryptobot/config/KafkaConfig.java
@@ -56,6 +56,8 @@ public class KafkaConfig {
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 250);
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,600000);
         props.put("security.protocol", "SASL_SSL");
         props.put("sasl.mechanism", "SCRAM-SHA-256");
         props.put("sasl.jaas.config", getJaas());
